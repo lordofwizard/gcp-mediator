809d30391e879c2b6cc471076b36a6f083ae4524
lordofwizard <advaitpandharpurkar619@gmail.com>
2024-06-03 13:06:00 +0530

    [TESTING REQ] Added client script failout

--- client.py
+++ client.py
@@ -29,27 +29,32 @@ client_socket.settimeout(5)
 fps,st,frames_to_count,cnt = (0,0,20,0)
 
 while True:
-    packet,(add,pt) = client_socket.recvfrom(BUFF_SIZE)
-    #print(f"add={add},pt={pt}")
-    #if packet:
-        #print("frame recieverd")
-    data = base64.b64decode(packet,' /')
-    npdata = np.fromstring(data,dtype=np.uint8)
-    frame = cv2.imdecode(npdata,1)
-    frame = cv2.putText(frame,'FPS: '+str(fps),(10,40),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
-    cv2.imshow("RECEIVING VIDEO",frame)
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord('q'):
-        client_socket.close()
-        break
-    if cnt == frames_to_count:
-        try:
-            fps = round(frames_to_count/(time.time()-st))
-            st=time.time()
-            cnt=0
-        except:
-            pass
-    cnt+=1
+    try:
 
+        packet,(add,pt) = client_socket.recvfrom(BUFF_SIZE)
+        #print(f"add={add},pt={pt}")
+        #if packet:
+            #print("frame recieverd")
+        data = base64.b64decode(packet,' /')
+        npdata = np.fromstring(data,dtype=np.uint8)
+        frame = cv2.imdecode(npdata,1)
+        frame = cv2.putText(frame,'FPS: '+str(fps),(10,40),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+        cv2.imshow("RECEIVING VIDEO",frame)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):
+            client_socket.close()
+            break
+        if cnt == frames_to_count:
+            try:
+                fps = round(frames_to_count/(time.time()-st))
+                st=time.time()
+                cnt=0
+            except:
+                pass
+        cnt+=1
+    except socket.timeout:
+        pass
+    except Exception as e:
+        print(f"Exception occured. as {e}")
 
 
